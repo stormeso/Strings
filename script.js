286abc98c553be3bd8ab19a0f7e16296b76b5f77
stormeso <torques.sophia@escola.pr.gov.br>
2025-07-02 23:36:53 +0000

    finalizando

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
-
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+document.addEventListener('DOMContentLoaded', () => {
+    const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+    botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+});
 
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
